64b115ceb26981d1e70161812a6487b777bbfdea
bananakiu <leandrenoelkiu@gmail.com>
2021-11-10 03:23:44 +0800

    fix errors related to notice js rendering

--- app/javascript/packs/notices.js
+++ app/javascript/packs/notices.js
@@ -1,4 +1,8 @@
-const closeButton = document.querySelector(".close-button");
-closeButton.addEventListener("click", () => {
+// set timeout is a workaround, because we have yet to learn AJAX
+setTimeout(function(){
+    const closeButton = document.querySelector(".close-button");
+    closeButton.addEventListener("click", () => {
+    console.log("clicked")
     document.querySelector(".notice").classList.add("hidden");
 })
+}, 100);
