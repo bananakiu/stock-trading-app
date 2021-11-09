af32eb481bd282bf3b26f820c2efb87433661d3c
Mikyle Reyes <reyesmikyle17@gmail.com>
2021-11-09 19:18:50 +0800

    Merge conflicts

--- app/controllers/stock_controller.rb
+++
@@ -1,6 +0,0 @@
-class StockController < ApplicationController
-  def show 
-    
-  end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,11 @@ Rails.application.routes.draw do
   resources :transactions
   get 'portfolio' => 'welcome#portfolio'
 
-  namespace :admin do
-    resources :users, except: :create do
-      # get 'stocks/show', to: 'stocks#show'
-      resources :stock, only: [:show]
-    end
+  get 'stocks', to: 'stocks#index'
+  get 'stocks/:ticker', to: 'stocks#show'
 
+  namespace :admin do
+    resources :users, except: :create
     post 'create_user' => 'users#create', as: :create_user
     patch 'update_user/:id', to: 'users#update', as: :update_user
     delete 'delete_user/:id' => 'users#destory', as: :delete_user
