7c219a0a70af3973d5157f0d5d5f1a04af696453
Mikyle Reyes <reyesmikyle17@gmail.com>
2021-11-10 12:59:48 +0800

    Change domain from 'localhost' to 'gmail.com' according to Sir Abdul

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     user_name: Rails.application.credentials.sendmail[:username],
     password: Rails.application.credentials.sendmail[:password],
-    domain: 'localhost',
+    domain: 'gmail.com',
     address: 'smtp.gmail.com',
     port: '587',
     authentication: :plain,
