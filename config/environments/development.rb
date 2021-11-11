03216cb8aa519c5f45d95fd8bebb80719d0b8f67
abdulmagundacan <83185067+abdulmagundacan@users.noreply.github.com>
2021-11-11 18:48:20 +0800

    Use encrypted credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -83,8 +83,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    user_name:            ENV["GMAIL_USERNAME"],
-    password:             ENV["GMAIL_PASSWORD"],
+    user_name:            Rails.application.credentials.sendmail[:username],
+    password:             Rails.application.credentials.sendmail[:password],
     authentication:       'plain',
     enable_starttls_auto: true }
 end
