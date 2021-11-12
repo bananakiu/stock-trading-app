83b9153ae91780a9b76396713e6a59d8a5e94f72
bananakiu <leandrenoelkiu@gmail.com>
2021-11-12 19:30:52 +0800

    improve confirmation checker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_user! # temporary. find out how to allow users to see landing page
 
     def needs_confirmation
-        flash[:alert] = "Only confirmed users can transact. Please confirm your email."
-        redirect_to root_path unless current_user.confirmed?
+        if current_user.confirmed?
+        else
+            flash[:alert] = "Only confirmed users can transact. Please confirm your email."
+            redirect_to root_path
+        end
     end
 end
