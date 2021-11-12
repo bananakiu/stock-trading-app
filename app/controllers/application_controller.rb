2f6449f4afb12f630d21b132afd6ac9123c94417
bananakiu <leandrenoelkiu@gmail.com>
2021-11-12 19:29:26 +0800

    modify confirmation logic (allow unconfirmed users to login; disallow unconfirmed users from  making transactions)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user! # temporary. find out how to allow users to see landing page
+
+    def needs_confirmation
+        flash[:alert] = "Only confirmed users can transact. Please confirm your email."
+        redirect_to root_path unless current_user.confirmed?
+    end
 end
