2e104ed2d6bcb4037435968022d85133e79c737f
bananakiu <leandrenoelkiu@gmail.com>
2021-11-12 21:08:16 +0800

    rename needs_confirmation method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user! # temporary. find out how to allow users to see landing page
 
-    def needs_confirmation
+    def needs_approval
         if current_user.approved == true
         else
             flash[:alert] = "Only approved users can transact. Please wait to be approved by an admin."
