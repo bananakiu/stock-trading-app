adfad11ba8e9fd25a7ba7c906d9f4d2e48aeb872
bananakiu <leandrenoelkiu@gmail.com>
2021-11-12 21:04:29 +0800

    disallow unapproved users from trading instead of unconfirmed users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_user! # temporary. find out how to allow users to see landing page
 
     def needs_confirmation
-        if current_user.confirmed?
+        if current_user.approved == true
         else
-            flash[:alert] = "Only confirmed users can transact. Please confirm your email."
+            flash[:alert] = "Only approved users can transact. Please wait to be approved by an admin."
             redirect_to root_path
         end
     end
