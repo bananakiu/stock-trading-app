e0dcae459888f5972dc3cfe64917c2370f7f9a80
bananakiu <leandrenoelkiu@gmail.com>
2021-11-13 13:13:44 +0800

    change timezone to default to local timezone

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module StockTradingApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_record.default_timezone = :local
   end
 end
