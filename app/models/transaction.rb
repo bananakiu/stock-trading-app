502b28801ea6dd6bc8c760aff752ecc1ee1201eb
bananakiu <leandrenoelkiu@gmail.com>
2021-11-20 03:29:33 +0800

    add validations to transaction action and price_per_share

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,7 +2,8 @@ class Transaction < ApplicationRecord
   belongs_to :user
 
   validates :stock, presence: true
-  validates :action, presence: true
+  validates :action, inclusion: { in: %w[Buy Sell] }
   validates :shares, presence: true, numericality: { greater_than: 0 }
+  validates :price_per_share, presence: true
 
 end
