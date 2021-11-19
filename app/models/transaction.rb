abcc73e3f5f597a49213ef3fff8f70fde82886f8
Mikyle Reyes <reyesmikyle17@gmail.com>
2021-11-19 21:15:48 +0800

    added validatoins to transactions model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,8 @@
 class Transaction < ApplicationRecord
   belongs_to :user
+
+  validates :stock, presence: true
+  validates :action, presence: true
+  # validates :shares, presence: true, numericality: { greater_than: 0 }
+
 end
