8663ef706e3550f2b78ac7dffea9bee77b80c483
bananakiu <leandrenoelkiu@gmail.com>
2021-11-20 12:00:28 +0800

    add fixtures; add user login test

--- spec/system/user_flows_spec.rb
+++ spec/system/user_flows_spec.rb
@@ -3,11 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe "Spec for Sign Up", type: :system do   
+    fixtures :users
+    
     before do
         driven_by(:rack_test)
     end
 
-    it "should create new user account" do     
+    it "should create new user account" do   
         visit new_user_registration_path
         fill_in 'user_first_name', :with => "Ab"
         fill_in 'user_last_name', :with => "Cd"
@@ -16,5 +18,13 @@ RSpec.describe "Spec for Sign Up", type: :system do
         fill_in 'user_password_confirmation', :with => "password"    
         click_button 'Sign up'    
         expect(page).to have_content "Welcome! You have signed up successfully."
-    end    
+    end
+
+    it "should log in existing user account" do     
+        visit new_user_session_path
+        fill_in 'user_email', :with => users(:one).email
+        fill_in 'user_password', :with => "password"
+        click_button 'Log in'    
+        expect(page).to have_content "Signed in successfully."
+    end
 end
