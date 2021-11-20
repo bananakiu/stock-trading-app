04926310cea19c515f389c9b912fc2922ae55722
Mikyle Reyes <reyesmikyle17@gmail.com>
2021-11-20 12:09:51 +0800

    add tests to transactions model

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,9 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, :type => :model do
-  it "is valid with valid attributes"
-  it "is not valid without a title"
-  it "is not valid without a description"
-  it "is not valid without a start_date"
-  it "is not valid without a end_date"
+  fixtures :users
+
+  it "is valid with valid attributes" do
+    transaction = users(:two).transactions.new(
+      stock: "TEST",
+      action: "Buy",
+      shares: 1,
+      price_per_share: 0
+    )
+    expect(transaction).to be_valid
+  end
+  it "is not valid without a stock" do
+    transaction = users(:two).transactions.new(
+      stock: nil,
+      action: "Buy",
+      shares: 1,
+      price_per_share: 0
+    )
+    expect(transaction).to_not be_valid
+  end
+  it "is not valid without an action" do
+    transaction = users(:two).transactions.new(
+      stock: "TEST",
+      action: "Buyz",
+      shares: 1,
+      price_per_share: 0
+    )
+    expect(transaction).to_not be_valid
+  end
+  it "is not valid with shares less than 0" do
+    transaction = users(:two).transactions.new(
+      stock: "TEST",
+      action: "Buy",
+      shares: -1,
+      price_per_share: 0
+    )
+    expect(transaction).to_not be_valid
+  end
+  it "is not valid without price_per_share" do
+    transaction = users(:two).transactions.new(
+      stock: "TEST",
+      action: "Buy",
+      shares: 1,
+      price_per_share: nil
+    )
+    expect(transaction).to_not be_valid
+  end
 end
