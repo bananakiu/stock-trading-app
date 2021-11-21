d5e78c1e94dec27419cfeecd774ff86fab7bb256
bananakiu <leandrenoelkiu@gmail.com>
2021-11-21 17:16:03 +0800

    remove error-filed migration file

--- db/migrate/20211119123044_remove_approved_column.rb
+++
@@ -1,8 +0,0 @@
-class RemoveApprovedColumn < ActiveRecord::Migration[6.1]
-  def up
-    remove_column :users, :confirmed, :boolean
-  end
-  def down
-    add_column :users, :confirmed, :boolean
-  end
-end
